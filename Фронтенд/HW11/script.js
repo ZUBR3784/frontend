4e89a5fc337ff52f4416f9e8c88e9cac189ac841
ZUBR3784 <114622156+ZUBR3784@users.noreply.github.com>
2023-02-26 18:40:06 +0100

    HW11

---
+++ Фронтенд/HW11/script.js
@@ -0,0 +1,33 @@
+// // Создать каунтер. Страница с двумя кнопками
+// //  “+” и “-” и при нажатии на
+// // какую-либо число должно меняться в соответствующую сторону. 
+// // Значение counter не должно слетать при перезагрузке страницы.
+// const body = document.body;
+// const form=document.querySelector('#form');
+// const input=document.querySelector('#input');
+// const addBtn = document.querySelector('#add')
+// const minusBtn= document.querySelector('#minus')
+// let numOfInp = +input.textContent;debugger
+
+// form.onsubmit=(e)=>{
+//     e.preventDefault();
+//     localStorage.setItem("num", input.value);  debugger
+// }
+
+// addBtn.onclick=()=>{
+//     numOfInp+=1;debugger
+//     input.value = numOfInp; debugger
+// }
+
+// minusBtn.onclick=()=>{
+//     numOfInp-=1;debugger
+//     input.value = numOfInp ;debugger
+// }
+
+// const num = localStorage.getItem('num')
+// if (num!=='') {
+//     input.value=num;debugger
+// }
+
+
+
